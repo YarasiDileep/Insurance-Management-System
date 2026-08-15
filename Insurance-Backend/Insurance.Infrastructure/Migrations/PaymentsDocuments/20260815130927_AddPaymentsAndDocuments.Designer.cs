7045d278f3f1a208623e60e77d25c00120678478
YarasiDileep <yarasidileep1@gmail.com>
2026-08-15 18:41:21 +0530

    Add EF Core migration: AddPaymentsAndDocuments

--- Insurance-Backend/Insurance.Infrastructure/Migrations/PaymentsDocuments/20260815130927_AddPaymentsAndDocuments.Designer.cs
+++ Insurance-Backend/Insurance.Infrastructure/Migrations/PaymentsDocuments/20260815130927_AddPaymentsAndDocuments.Designer.cs
@@ -4,16 +4,19 @@ using Insurance.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace Insurance.Infrastructure.Migrations
+namespace Insurance.Infrastructure.Migrations.PaymentsDocuments
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260815130927_AddPaymentsAndDocuments")]
+    partial class AddPaymentsAndDocuments
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
