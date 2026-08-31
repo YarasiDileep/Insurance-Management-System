eeefca7cff42d3bfb725e387c0bf90426edf28d1
YarasiDileep <yarasidileep1@gmail.com>
2026-08-31 19:51:33 +0530

    test(token): add unit tests for TokenInterceptor

---
+++ insurance-frontend/src/app/core/token.interceptor.spec.ts
@@ -0,0 +1,53 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptor, provideTokenInterceptor } from './token.interceptor';
+import { AuthService } from './auth.service';
+
+class MockAuthService {
+  token: string | null = null;
+  getToken() { return this.token; }
+}
+
+describe('TokenInterceptor', () => {
+  let http: HttpClient;
+  let httpMock: HttpTestingController;
+  let auth: MockAuthService;
+
+  beforeEach(() => {
+    auth = new MockAuthService();
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        { provide: AuthService, useValue: auth },
+        provideTokenInterceptor
+      ]
+    });
+
+    http = TestBed.inject(HttpClient);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => httpMock.verify());
+
+  it('should NOT add Authorization header when no token', (done) => {
+    auth.token = null;
+
+    http.get('/api/Customers').subscribe(() => done());
+
+    const req = httpMock.expectOne('/api/Customers');
+    expect(req.request.headers.has('Authorization')).toBeFalse();
+    req.flush([]);
+  });
+
+  it('should add Authorization header when token present', (done) => {
+    auth.token = 'abc123';
+
+    http.get('/api/Customers').subscribe(() => done());
+
+    const req = httpMock.expectOne('/api/Customers');
+    expect(req.request.headers.get('Authorization')).toBe('Bearer abc123');
+    req.flush([]);
+  });
+});
